b8c5a78a757f289d464927d7f618f076331207ac
Paddy <pacurtin@tcd.ie>
2017-02-04 20:37:08 +0000

    First three chapters of progress

---
+++ app/src/main/java/com/example/padraigcurtin/criminalintent/Crime.java
@@ -0,0 +1,53 @@
+package com.example.padraigcurtin.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by padraig.curtin on 04/02/2017.
+ */
+
+public class Crime {
+    private Date mDate;
+    private boolean mSolved;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    private UUID mId;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    private String mTitle;
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+    public void setDate(Date date) {
+        mDate = date;
+    }
+    public boolean isSolved() {
+        return mSolved;
+    }
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    @Override
+    public String toString() {
+        return mTitle;
+    }
+
+}
---
+++ app/src/main/java/com/example/padraigcurtin/criminalintent/CrimeActivity.java
@@ -0,0 +1,15 @@
+package com.example.padraigcurtin.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+
+public class CrimeActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+
+}
